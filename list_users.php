758a6565dfb2668ff0d8e62cc72320a92f6c7e9e
ElenaSirbu <62076797+ElenaSirbu@users.noreply.github.com>
2025-02-15 13:10:51 +0200

    login/logout

--- list_users.php
+++ list_users.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 
-if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'administrator') {
+/*if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'administrator') {
     header("Location: login.php");
     exit;
-}
+}*/
 // Verificăm dacă utilizatorul este autentificat
 if (!isset($_SESSION['user_id'])) {
     header("Location: login.php");
